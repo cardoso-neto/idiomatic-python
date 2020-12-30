e4e91b3cf0489f22d6916a0c0c294e417f421f03
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-12-30 10:56:58 -0300

    Add hidden variable wildcard import example

--- 2020.12.10/underscores/main.py
+++ 2020.12.10/underscores/main.py
@@ -3,6 +3,7 @@ from module1 import *
 
 print(a)
 # 4
-
+print(_b)
+# NameError: name '_b' is not defined
 _function()
 # NameError: name '_function' is not defined
--- 2020.12.10/underscores/module1.py
+++ 2020.12.10/underscores/module1.py
@@ -3,3 +3,4 @@ def _function():
     print('ass')
 
 a = 4
+_b = 3
